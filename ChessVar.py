97d64aeea45f031b896bf501defedd0cb0bbb8c9
jovanny-macbookair <jovannygochez23@gmail.com>
2023-09-26 21:39:14 -0500

    Completed a chess variant game with full UI and engine.

--- ChessVar.py
+++ ChessVar.py
@@ -16,22 +16,23 @@ class ChessVar:
         initializes the data members, and sets them as private.
         """
 
-        self._referenceBoard = [['a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8'],
-                                ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'],
-                                ['a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6'],
-                                ['a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5'],
-                                ['a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4'],
-                                ['a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3'],
-                                ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'],
-                                ['a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1']]
-        self._liveBoard = [['___', '___', '___', '___', '___', '___', '___', '___'],
-                           ['___', '___', '___', '___', '___', '___', '___', '___'],
-                           ['___', '___', '___', '___', '___', '___', '___', '___'],
-                           ['___', '___', '___', '___', '___', '___', '___', '___'],
-                           ['___', '___', '___', '___', '___', '___', '___', '___'],
-                           ['___', '___', '___', '___', '___', '___', '___', '___'],
-                           ['W_R', 'W1B', 'W1N', '___', '___', 'B1N', 'B1B', 'B_R'],
-                           ['W_K', 'W2B', 'W2N', '___', '___', 'B2N', 'B2B', 'B_K']]
+        self._referenceBoard = [['N/A', 'a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8'],
+                                ['N/A', 'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'],
+                                ['N/A', 'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6'],
+                                ['N/A', 'a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5'],
+                                ['N/A', 'a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4'],
+                                ['N/A', 'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3'],
+                                ['N/A', 'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'],
+                                ['N/A', 'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1']]
+        self._liveBoard = [[' 8 ', '___', '___', '___', '___', '___', '___', '___', '___'],
+                           [' 7 ', '___', '___', '___', '___', '___', '___', '___', '___'],
+                           [' 6 ', '___', '___', '___', '___', '___', '___', '___', '___'],
+                           [' 5 ', '___', '___', '___', '___', '___', '___', '___', '___'],
+                           [' 4 ', '___', '___', '___', '___', '___', '___', '___', '___'],
+                           [' 3 ', '___', '___', '___', '___', '___', '___', '___', '___'],
+                           [' 2 ', 'W_R', 'W1B', 'W1N', '___', '___', 'B1N', 'B1B', 'B_R'],
+                           [' 1 ', 'W_K', 'W2B', 'W2N', '___', '___', 'B2N', 'B2B', 'B_K'],
+                           ['   ', ' a ', ' b ', ' c ', ' d ', ' e ', ' f ', ' g ', ' h ']]
         self._board_row = None
         self._board_column = None
         self._check_for_player = 0
@@ -466,7 +467,6 @@ class ChessVar:
                                     else:
                                         print("Player BLACK just moved.")
                                         self._check_for_player += 1
-                                        self.check_for_winner()
                                 else:
                                     print("Invalid move, you have a piece in that desired location.")
                             else:
@@ -510,7 +510,6 @@ class ChessVar:
                                     else:
                                         print("Player BLACK just moved.")
                                         self._check_for_player += 1
-                                        self.check_for_winner()
                                 else:
                                     print("Invalid move, you have a piece in that desired location.")
                             else:
@@ -551,7 +550,6 @@ class ChessVar:
                                 else:
                                     print("Player BLACK just moved.")
                                     self._check_for_player += 1
-                                    self.check_for_winner()
                             else:
                                 print("Invalid move, you have a piece in that desired location.")
                         else:
@@ -589,7 +587,6 @@ class ChessVar:
                                 else:
                                     print("Player BLACK just moved.")
                                     self._check_for_player += 1
-                                    self.check_for_winner()
                             else:
                                 print("Invalid move, you have a piece in that desired location.")
                         else:
